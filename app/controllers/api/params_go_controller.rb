c91c5bcd079834a69db6ce539468dc5ebf5e8e26
David Quaggin-Smith <quagginsmith@gmail.com>
2021-03-01 10:47:15 -0600

    init

--- app/controllers/api/params_go_controller.rb
+++ app/controllers/api/params_go_controller.rb
@@ -1,2 +1,7 @@
 class Api::ParamsGoController < ApplicationController
+  def params_query_action
+    input_phrase = params[:phrase].upcase
+    @output = input_phrase
+    render "params_test.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/params_query_path" => "params_go#params_query_action"
+    get "/params_query_path/:phrase" => "params_go#params_query_action"
+  end
 end
